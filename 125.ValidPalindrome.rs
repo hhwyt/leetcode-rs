f44a96bf92b2ee33f18e666fa2bb25cf106e9756
hhwyt <hhwyt1@gmail.com>
2019-08-02 00:34:52 +0800

    valid palindrome

---
+++ 125.ValidPalindrome.rs
@@ -0,0 +1,49 @@
+#![allow(dead_code)]
+struct Solution;
+
+impl Solution {
+    pub fn is_palindrome(s: String) -> bool {
+        if s.is_empty() {
+            return true;
+        }
+        let s = s.as_bytes();
+        let mut i = 0;
+        let mut j = s.len() - 1;
+        while i < j {
+            if !s[i].is_ascii_alphanumeric() {
+                i += 1;
+                continue;
+            }
+            if !s[j].is_ascii_alphanumeric() {
+                j -= 1;
+                continue;
+            }
+            if s[i].to_ascii_lowercase() != s[j].to_ascii_lowercase() {
+                return false;
+            }
+            i += 1;
+            j -= 1;
+        }
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let s0 = String::from("");
+        assert_eq!(Solution::is_palindrome(s0), true);
+
+        let s1 = String::from("A man, a plan, a canal Panama");
+        assert_eq!(Solution::is_palindrome(s1), true);
+
+        let s2 = String::from("race a car");
+        assert_eq!(Solution::is_palindrome(s2), false);
+
+        let s3 = String::from("0P");
+        assert_eq!(Solution::is_palindrome(s3), false);
+    }
+}

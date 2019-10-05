95be0eaf3286307e103bbdf8fda26a157480d6b0
hhwyt <hhwyt1@gmail.com>
2019-10-05 21:37:32 +0800

    Solve #139

---
+++ 139.WordBreak-Recursive-2.rs
@@ -0,0 +1,56 @@
+#![allow(dead_code)]
+
+struct Solution;
+
+impl Solution {
+    fn word_break_recursive(s: &str, start: i32, end: i32, word_dict: &Vec<String>) -> bool {
+        if start == end {
+            return true;
+        }
+
+        for word in word_dict {
+            if let Some(pos) = s[start as usize..end as usize].find(word) {
+                if Self::word_break_recursive(&s, start, start + pos as i32, word_dict)
+                    && Self::word_break_recursive(&s, start + (pos  + word.len()) as i32, end, word_dict) {
+                    return true;
+                }
+            }
+        }
+
+        false
+    }
+
+    pub fn word_break(s: String, word_dict: Vec<String>) -> bool {
+        return Self::word_break_recursive(&s, 0, s.len() as i32, &word_dict);
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_normal() {
+        let s = "leetcode".to_owned();
+        let word_dict = vec!["leet".to_owned(), "code".to_owned()];
+        let expected = true;
+        assert_eq!(Solution::word_break(s, word_dict), expected);
+    }
+
+    #[test]
+    fn test_duplicate() {
+        let s = "applepenapple".to_owned();
+        let word_dict = vec!["apple".to_owned(), "pen".to_owned()];
+        let expected = true;
+        assert_eq!(Solution::word_break(s, word_dict), expected);
+    }
+
+    #[test]
+    fn test_false() {
+        let s = "catsandog".to_owned();
+        let word_dict = vec!["cats".to_owned(), "dog".to_owned(), "sand".to_owned(), "and".to_owned(), "cat".to_owned()];
+        let expected = false;
+        assert_eq!(Solution::word_break(s, word_dict), expected);
+    }
+}

fa04e8408577c4c430ebfdaed371a9c76d1c6625
hhwyt <hhwyt1@gmail.com>
2019-10-15 22:08:45 +0800

    Morris traversal for preorder and postorder

---
+++ 00145.BinaryTreePostorderTraversal-Morris.rs
@@ -0,0 +1,94 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+struct TreeNode {
+    val: i32,
+    left: Option<Rc<RefCell<TreeNode>>>,
+    right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    pub fn new(val: i32) -> TreeNode {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn postorder_traversal(mut some_root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut res = Vec::new();
+        while let Some(rc_root) = some_root {
+            let root = rc_root.borrow();
+            match root.right.clone() {
+                Some(mut cur) => {
+                    while cur.borrow().left.is_some() && !Rc::ptr_eq(&cur.borrow().left.clone().unwrap(), &rc_root) {
+                        cur = cur.clone().borrow().left.clone().unwrap();
+                    }
+
+                    if cur.borrow().left.is_none() {
+                        res.push(root.val);
+                        cur.borrow_mut().left = Some(rc_root.clone());
+                        some_root = root.right.clone();
+                    } else {
+                        cur.borrow_mut().left = None;
+                        some_root = root.left.clone();
+                    }
+                }
+                None => {
+                    res.push(root.val);
+                    some_root = root.left.clone();
+                }
+            }
+        }
+        res.reverse();
+        res
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn vec_2_tree_recursive(v: &Vec<Option<i32>>, cur: usize) -> Option<Rc<RefCell<TreeNode>>> {
+        if v.get(cur).is_none() || v.get(cur).unwrap().is_none() {
+            return None;
+        }
+
+        // left_child = parent * 2 + 1
+        let mut root = TreeNode::new(v.get(cur).unwrap().unwrap());
+        let left_child = cur * 2 + 1;
+        root.left = tests::vec_2_tree_recursive(v, left_child);
+        root.right = tests::vec_2_tree_recursive(v, left_child + 1);
+        Some(Rc::new(RefCell::new(root)))
+    }
+
+    fn vec_2_tree(v: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
+        return tests::vec_2_tree_recursive(v.as_ref(), 0);
+    }
+
+    #[test]
+    fn test_normal() {
+        /*
+             1
+            / \
+           2   3
+        */
+        let v = vec![Some(1), Some(2), Some(3)];
+        let root = tests::vec_2_tree(v);
+        let expected = vec![2, 3, 1];
+        assert_eq!(Solution::postorder_traversal(root), expected);
+    }
+
+    #[test]
+    fn test_empty_tree() {
+        let v = vec![];
+        let root = tests::vec_2_tree(v);
+        let expected: Vec<i32> = vec![];
+        assert_eq!(Solution::postorder_traversal(root), expected);
+    }
+}
